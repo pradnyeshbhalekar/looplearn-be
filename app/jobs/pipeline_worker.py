13498fcbaeb4c17cb58427c5ed705dd37ab21fdb
Pradnyesh <pradnyeshbhalekar78@gmail.com>
2026-03-05 19:50:31 +0530

    feat: added email to the pipeline

--- app/jobs/pipeline_worker.py
+++ app/jobs/pipeline_worker.py
@@ -2,6 +2,8 @@ import threading
 import uuid
 from app.jobs.job_store import jobs,JobStatus
 from app.services.pipeline_service import run_pipeline
+from app.services.email_service import send_admin_notification
+from app.utils.email_utils import get_admin_emails
 
 def start_pipeline_job():
     job_id = str(uuid.uuid4())
@@ -22,15 +24,21 @@ def start_pipeline_job():
 
     return job_id
 
-
-
-
-def _run_pipeline_job(job_id:str):
+def _run_pipeline_job(job_id: str):
     jobs[job_id]['status'] = JobStatus.RUNNING
+
     try:
         result = run_pipeline()
+
         jobs[job_id]['status'] = JobStatus.COMPLETED
         jobs[job_id]['result'] = result
+
+        topics = result.get("topics", [])
+
+        if topics:
+            emails = get_admin_emails()
+            send_admin_notification(emails, topics)
+
     except Exception as e:
         jobs[job_id]['status'] = JobStatus.FAILED
         jobs[job_id]['error'] = str(e)
