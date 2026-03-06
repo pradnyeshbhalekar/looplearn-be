4c40089dc7fd8ce07eb9c6d5316800d300202357
Pradnyesh <pradnyeshbhalekar78@gmail.com>
2026-03-07 00:19:05 +0530

    email auth changes

--- app/utils/email_utils.py
+++ app/utils/email_utils.py
@@ -2,9 +2,12 @@ from app.config.db import get_connection,close_connection
 
 def get_admin_emails():
     conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("""
-                   SELECT u.email FROM users u JOIN user_roles ur ON u.id = ur.user_id WHERE ur.role = 'admin'
-                   """)
-    rows = cursor.fetchall()
-    return [r[0] for r in rows]
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT u.email FROM users u JOIN user_roles ur ON u.id = ur.user_id WHERE ur.role = 'admin'
+        """)
+        rows = cursor.fetchall()
+        return [r[0] for r in rows]
+    finally:
+        close_connection(conn)
